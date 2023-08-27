ed62c60183f457efe9d1bd098db79704b376adca
Developer Experience team at Ignite <hello@ignite.com>
2023-08-27 12:32:00 +0530

    Initialized with Ignite CLI

---
+++ x/chat/keeper/msg_server_test.go
@@ -0,0 +1,23 @@
+package keeper_test
+
+import (
+	"context"
+	"testing"
+
+	keepertest "chat/testutil/keeper"
+	"chat/x/chat/keeper"
+	"chat/x/chat/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+)
+
+func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
+	k, ctx := keepertest.ChatKeeper(t)
+	return keeper.NewMsgServerImpl(*k), sdk.WrapSDKContext(ctx)
+}
+
+func TestMsgServer(t *testing.T) {
+	ms, ctx := setupMsgServer(t)
+	require.NotNil(t, ms)
+	require.NotNil(t, ctx)
+}
